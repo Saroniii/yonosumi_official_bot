2b73aa0917c05001e98a90f64fba6b4f8c595b20
Saroniii <sarotube0211@gmail.com>
2020-09-23 10:20:31 +0900

    [Del]未使用モジュールの一括削除

--- module/eval.py
+++ module/eval.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from contextlib import redirect_stdout
-import ast
 import io
 import textwrap
 import traceback
--- module/private_channel.py
+++ module/private_channel.py
@@ -1,4 +1,3 @@
-from types import new_class
 import discord
 from discord.ext import commands
 
