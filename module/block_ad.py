3518bd03074455b09af431780058d1421dedcc5d
Saroniii <sarotube0211@gmail.com>
2020-10-08 20:20:42 +0900

    [Add]オススメ宣伝内での重複投稿を検知するように

---
+++ module/block_ad.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+
+class Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message :discord.Message):
+        ad_channel_id = 572737509923225600
+        if message.author.bot or not message.channel.id == ad_channel_id:
+            return
+        
+        msg :discord.Message 
+
+        async for msg in message.channel.history(limit=25, oldest_first=True):
+            if msg.content == message.content and not msg.id == message.id:
+                await message.delete()
+                content = f"{message.guild}の{message.channel.mention}での重複投稿を検知したため、対象の投稿を削除しました。"
+                embed = discord.Embed(
+                        title = "検知した過去の投稿",
+                        description = f"{msg.content}\n\n[対象の投稿に飛ぶ]({msg.jump_url})",
+                        timestamp = msg.created_at
+                    ).set_footer(text=f"該当メッセージの投稿日")
+                await message.author.send(content, embed=embed)
+                logch = self.bot.get_channel(763722418182684672)
+                await logch.send(content, embed=embed)
+                break
+
+
+def setup(bot):
+    bot.add_cog(Cog(bot))
