3edc3fe5b2ff79af5d3c816301a6547a129a5571
Saroniii <sarotube0211@gmail.com>
2020-10-26 14:30:53 +0900

    [Fix]関数が正常に動作しなかったバグを修正

--- module/autovc.py
+++ module/autovc.py
@@ -11,7 +11,7 @@ class Cog(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member :discord.Member, before :discord.VoiceState, after :discord.VoiceState):
         
-        if self.voice.is_active(member.voice.channel, count_bots = False) and self.voice.is_generate_voice_channel:
+        if self.voice.is_active(member.voice.channel, count_bots = False) and self.voice.is_generate_voice_channel(member.voice.channel):
             author_channel :discord.VoiceChannel = member.voice.channel
             voicechannel: discord.VoiceChannel = await author_channel.category.create_voice_channel(
                 name=f"{member.name}の溜まり場"
@@ -26,12 +26,12 @@ class Cog(commands.Cog):
             await member.move_to(voicechannel, reason = "VCの自動生成が完了したため")
             base_embed = discord.Embed(
                 title = f"{member.name}の溜まり場へようこそ！",
-                description = f"ここでは、該当するリアクションを押すことで様々な設定を行うことが出来ます。\n\n✎：チャンネル名の変更\n\n🔒：利用可能人数の制限\n\n⚠：NSFWの有無"
+                description = f"ここでは、該当するリアクションを押すことで様々な設定を行うことが出来ます。\n\n✏：チャンネル名の変更\n\n🔒：利用可能人数の制限\n\n⚠：NSFWの有無"
             )
             
             msg: discord.Message = await textchannel.send(embed=base_embed)
             
-            reaction_list = ["✎", "🔒", "⚠"]
+            reaction_list = ["✏", "🔒", "⚠"]
 
             for emoji in reaction_list:
                 await msg.add_reaction(emoji)
