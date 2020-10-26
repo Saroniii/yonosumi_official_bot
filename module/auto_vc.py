fd14c4ce2bfe63551110135cc8e8cbecdf02481a
Saroniii <sarotube0211@gmail.com>
2020-10-26 17:53:42 +0900

    [Add]VCを自動生成するように

--- module/auto_vc.py
+++ module/auto_vc.py
@@ -1,6 +1,12 @@
+import re
 import discord
 from discord.ext import commands
 from yonosumi_utils import voice
+from asyncio import TimeoutError
+
+import yonosumi_utils
+
+reaction_list = ["✏", "🔒"]
 
 class Cog(commands.Cog):
 
@@ -20,10 +26,12 @@ class Cog(commands.Cog):
                 )
 
         elif self.voice.is_active(member.voice.channel, count_bots = False) and self.voice.is_generate_voice_channel(member.voice.channel):
+            
             author_channel :discord.VoiceChannel = member.voice.channel
             voicechannel: discord.VoiceChannel = await author_channel.category.create_voice_channel(
                 name=f"{member.name}の溜まり場"
                 )
+            
             textchannel: discord.TextChannel = await author_channel.category.create_text_channel(
                 name=f"{member.name}の溜まり場",
                 topic=self.voice.generate_auto_voice_topic(
@@ -31,19 +39,84 @@ class Cog(commands.Cog):
                     member=member
                     )
                 )
+            
             await member.move_to(voicechannel, reason = "VCの自動生成が完了したため")
             base_embed = discord.Embed(
                 title = f"{member.name}の溜まり場へようこそ！",
-                description = f"ここでは、該当するリアクションを押すことで様々な設定を行うことが出来ます。\n\n✏：チャンネル名の変更\n\n🔒：利用可能人数の制限\n\n⚠：NSFWの有無"
+                description = self.voice.base_embed_description()
             )
             
             msg: discord.Message = await textchannel.send(embed=base_embed)
             
-            reaction_list = ["✏", "🔒", "⚠"]
+            global reaction_list
 
             for emoji in reaction_list:
                 await msg.add_reaction(emoji)
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload :discord.RawReactionActionEvent):
+        
+        if payload.member.bot:
+            return 
+
+        channel :discord.TextChannel = self.bot.get_channel(payload.channel_id)
+        voice_category_id = 770140316078309416
+
+        if not self.voice.is_muted_text_channel(channel) or payload.member != await self.voice.get_auto_voice_owner(channel):
+            return
+
+        message :discord.Message = await channel.fetch_message(payload.message_id)
+
+        if not self.voice.is_voice_control_panel(message):
+            return
+        
+        global reaction_list
+
+        if not str(payload.emoji) in reaction_list:
+            return
+        
+        if str(payload.emoji) == "✏":
+            
+            question = await channel.send(f"{payload.member.mention}->変更したい名前を入力してください。")
+            
+            def check(m):
+                return m.channel == channel and m.author == payload.member
+            try:
+                msg = await self.bot.wait_for('message', check=check, timeout=60.0)
+            except TimeoutError:
+                return await question.edit(content=f"{payload.member.mention}->規定時間内に応答がなかったので入力待機を解除しました！")
+            except:
+                return
+            if len(msg.content) > 0:
+                await channel.edit(name=msg.content)
+                vc = await self.bot.get_channel(yonosumi_utils.get_topic(channel, splited=True)[1])
+                await vc.edit(name=msg.content)
+                await channel.send(f"{payload.member.mention}->チャンネル名を``{msg.content}``に変更しました！")
+
+        elif str(payload.emoji) == "🔒":
+            
+            question = await channel.send(f"{payload.member.mention}->このVCに入れる人数を指定してください。")
+            
+            def check(m):
+                return m.channel == channel and m.author == payload.member
+
+            try:
+                msg = await self.bot.wait_for('message', check=check, timeout=60.0)
+            except TimeoutError:
+                return await question.edit(content=f"{payload.member.mention}->規定時間内に応答がなかったので入力待機を解除しました！")
+            except:
+                return
+            try:
+                num = int(message.content)
+            except:
+                return await question.edit(content=f"{payload.member.mention}->不正な値が渡されました！")
+            if not num > 100:
+                vc: discord.VoiceChannel = await self.bot.get_channel(yonosumi_utils.get_topic(channel, splited=True)[1])
+                await vc.edit(user_limit=num)
+                await channel.send(f"{payload.member.mention}->VCの参加可能人数を``{num}人``に変更しました！")
+            else:
+                return await question.edit(content=f"{payload.member.mention}->100人以上は指定できません！")
+        
 
 def setup(bot):
     bot.add_cog(Cog(bot))
