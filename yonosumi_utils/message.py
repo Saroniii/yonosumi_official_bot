39886e63525a7b03d0ab0e29165cfc91ed7928fa
Saroniii <sarotube0211@gmail.com>
2020-10-29 20:04:06 +0900

    [Add]question関数の追加

--- yonosumi_utils/message.py
+++ yonosumi_utils/message.py
@@ -1,5 +1,7 @@
 import discord
+from discord.ext import commands
 from yonosumi_utils.embed import YonosumiEmbed
+from asyncio import TimeoutError
 
 class YonosumiMsg:
     
@@ -18,3 +20,19 @@ class YonosumiMsg:
     @staticmethod
     async def startup_process(ctx):
         return await ctx.send(f"{ctx.author.mention}->起動処理中です...再度お試しください...")
+
+    @staticmethod
+    async def question(bot :commands.Bot, ctx :commands.Context, title :str) -> discord.Message:
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+        question = await ctx.send(content = title)
+        try:
+            msg = await bot.wait_for(
+                'message',
+                check=check,
+                timeout=60.0
+                )
+        except TimeoutError:
+            await question.edit(content=f"{ctx.author.mention}->入力待機時間内に応答がありませんでした！")
+            return False
+        return msg
