97f294817ac213f1e49c37d2836cc7fe00d5e2e3
Saroniii <sarotube0211@gmail.com>
2020-10-29 18:11:34 +0900

    [Add]MEE6のレベルアップメッセージを検知して役職を付与するように

--- yonosumi_utils/__init__.py
+++ yonosumi_utils/__init__.py
@@ -3,3 +3,4 @@ from yonosumi_utils.message import *
 from yonosumi_utils.my_channel import *
 from yonosumi_utils.permissions import *
 from yonosumi_utils.voice import *
+from yonosumi_utils.level import *
