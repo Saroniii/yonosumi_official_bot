d3cd7351680a230619e1f8441ddc4a741ed39d2b
Saroniii <sarotube0211@gmail.com>
2020-11-10 17:55:27 +0900

    [Add]匿名送信機能の追加

---
+++ module/anonymous.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from yonosumi_utils import Anonymous as tokumei
+
+
+class Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+        self.tokumei = tokumei()
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+
+        if not type(message.channel) == discord.DMChannel:
+            return
+
+        dialog = await self.tokumei.setup_anonymous(
+            channel=message.channel,
+            message=message,
+        )
+
+        reaction = await self.tokumei.wait_answer(
+                channel=message.channel,
+                message=dialog,
+                bot=self.bot
+            )
+
+        channel_id = await self.tokumei.do_task(
+            message=message,
+            reaction=reaction
+        )
+
+        await self.tokumei.send_anonymous(
+            channel_id=channel_id,
+            bot=self.bot,
+            message=message
+        )
+
+        return await message.channel.send('メッセージを匿名で送信しました！')
+
+
+
+def setup(bot):
+    bot.add_cog(Cog(bot))
