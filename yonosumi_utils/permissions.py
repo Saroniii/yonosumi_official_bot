2dbc3c676dec77f0d69d0d8beefa40769e20ff94
Saroniii <sarotube0211@gmail.com>
2020-11-16 10:33:15 +0900

    evalが使用できないissueの修正

--- yonosumi_utils/permissions.py
+++ yonosumi_utils/permissions.py
@@ -5,9 +5,9 @@ from discord.ext import commands
 class YonosumiStaff:
 
     @staticmethod
-    def is_admin(bot: commands.Bot, ctx: commands.Context):
+    async def is_admin(bot: commands.Bot, ctx: commands.Context):
         try:
-            if [i for i in bot.get_guild(569928145906565126).get_member(ctx.author.id).roles if i.id == 579558148914806796]:
+            if [i for i in (await bot.get_guild(569928145906565126).fetch_member(ctx.author.id)).roles if i.id == 579558148914806796]:
                 return True
             else:
                 return False
