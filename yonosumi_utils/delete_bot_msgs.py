bfe5d388b4d7e4d64044ac680bb63e4449203e19
Saroniii <sarotube0211@gmail.com>
2020-11-19 23:23:58 +0900

    [Fix]引数不足のエラーを修正

--- yonosumi_utils/delete_bot_msgs.py
+++ yonosumi_utils/delete_bot_msgs.py
@@ -10,31 +10,31 @@ class DeleteMessage:
         self.level = level()
         pass
 
-    def can_delete_bot_message(self, message: discord.Message):
+    def can_delete_bot_message(self, message: discord.Message, bot: commands.Bot):
         """
         削除可能なBotのメッセージか判定します。
         """
-        if not message.author.id == 758126661509447701 or self.voice.is_voice_control_panel(message) or self.level.is_level_up_message(message):
+        if not message.author.id == 758126661509447701 or self.voice.is_voice_control_panel(message, bot) or self.level.is_level_up_message(message):
             return False 
         
         return True
 
-    async def add_trash_emoji(self, message: discord.Message):
+    async def add_trash_emoji(self, message: discord.Message, bot: commands.Bot):
         """
         ゴミ箱のリアクションを追加します。
         """
-        if not self.can_delete_bot_message(message):
+        if not self.can_delete_bot_message(message, bot):
             return False
         
         await message.add_reaction('🗑️')
 
         return True
 
-    async def delete_bot_message(self, message: discord.Message, reaction: str):
+    async def delete_bot_message(self, message: discord.Message, reaction: str, bot :commands.Bot):
         """
         Botのメッセージを削除します。
         """
-        if not self.can_delete_bot_message(message) or not reaction == "🗑️":
+        if not self.can_delete_bot_message(message, bot) or not reaction == "🗑️":
             return False
         
         await message.delete()
