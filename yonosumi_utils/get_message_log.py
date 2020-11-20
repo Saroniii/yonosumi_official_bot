939646cc845ab92b12dfcf57987b9195c6c4c1a2
Saroniii <sarotube0211@gmail.com>
2020-11-20 14:24:20 +0900

    [Add]メッセージ保持機能を動作させるためのクラスを作成

---
+++ yonosumi_utils/get_message_log.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+class GetMessageLog:
+
+    async def get_message_log(self, channel: discord.TextChannel, limit =None):
+        """
+        メッセージログを取得します。
+        """
+        log = ""
+        async for message in channel.history(limit=limit):
+            
+            log += f"{message.author}({message.author.id}){message.content}\n"
+
+            if message.attachments:
+                attachment_count = len(message.attachments)
+
+                for i in range(attachment_count):
+                    log += f"↪{message.attachments[i].url}\n"
+                
+        return log
+
+    def return_text_file(self, filename: str, sentence: str):
+        """
+        テキストファイルに書き込みます。
+        """
+        with open(filename, mode='w') as f:
+            f.write(sentence)
+            
+                
