c7b1df3f8b86d93c39aad779d7799b3eddb90f98
Saroniii <sarotube0211@gmail.com>
2020-11-20 23:07:29 +0900

    [Change]メッセージログに詳細な表記を追加

--- yonosumi_utils/get_message_log.py
+++ yonosumi_utils/get_message_log.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 class GetMessageLog:
 
@@ -7,16 +8,26 @@ class GetMessageLog:
         """
         メッセージログを取得します。
         """
-        log = ""
+        log = f"#{channel}のチャットログ"
+        message_count = 0
         async for message in channel.history(limit=limit, oldest_first=True):
             
             log += f"{message.author}({message.author.id}):{message.content}\n"
+            message_count += 1
 
             if message.attachments:
                 attachment_count = len(message.attachments)
 
                 for i in range(attachment_count):
                     log += f"↪{message.attachments[i].url}\n"
+        
+        log += f"\n総メッセージ数:{message_count}"
+        time = datetime.now()
+        log += f"""
+        \n\nこのメッセージログは、{time}時点の#{channel.name}のログを出力させたものです。\n
+        このメッセージログの捏造等を固く禁じます。\n
+        ©2020 Saroniii All rights rserved.
+        """
                 
         return log
 
