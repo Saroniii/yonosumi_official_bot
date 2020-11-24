cbe54cdc7f5ca3b73ae6d51ef3269ef363cb0751
Saroniii <sarotube0211@gmail.com>
2020-11-24 16:56:30 +0900

    [Change]寝床所有者か管理者のみがチャットログを保存できるように

--- module/get_message_log.py
+++ module/get_message_log.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-from yonosumi_utils import GetMessageLog as log
-from yonosumi_utils import YonosumiMsg as msg
+from yonosumi_utils import GetMessageLog as log, YonosumiMsg as msg, YonosumiStaff as staff
+import yonosumi_utils
 
 
 class Cog(commands.Cog):
@@ -9,10 +9,20 @@ class Cog(commands.Cog):
         self.bot = bot
         self.log = log()
         self.msg = msg()
+        self.staff = staff()
 
 
     @commands.command()
     async def get_message_log(self, ctx :commands.Bot, limit =None):
+        
+        if yonosumi_utils.is_nedoko(ctx.channel):
+            if not yonosumi_utils.check_owner(ctx.author, ctx.channel) or not self.staff.is_admin(self.bot, ctx):
+                return await ctx.send(f"{ctx.author.mention}->このコマンドは寝床所有者のみ使用できます！")
+        
+        else:
+            if not self.staff.is_admin(self.bot, ctx):
+                return await ctx.send(f"{ctx.author.mention}->このコマンドは寝床所有者のみ使用できます！")
+
         waiting = await self.msg.waiting(ctx)
         log = await self.log.get_message_log(channel=ctx.channel, limit=limit)
         filename = f"{ctx.channel.name}.txt"
