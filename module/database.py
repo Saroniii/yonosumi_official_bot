7c02dcc24c94c2aec8fc7c0f25617aca405b8ed9
Saroniii <sarotube0211@gmail.com>
2020-11-24 17:41:06 +0900

    [Fix]DBが読み込めないバグを修正

--- module/database.py
+++ module/database.py
@@ -7,11 +7,10 @@ class Cog(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
         self.loop.start()
-        self.dropbox = dropbox()
 
     @tasks.loop(minutes=5)
     async def loop(self):
-        self.dropbox.upload_database(
+        dropbox().upload_database(
             has_database=self.dropbox.has_database(self.dropbox.database_name),
             database_name=self.dropbox.database_name
         )
