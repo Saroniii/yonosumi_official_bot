18c3a23265a2c2d4ac74d530f795da00010b94d4
Saroniii <sarotube0211@gmail.com>
2021-01-17 11:34:30 +0900

    feat: create "create_nedoko" command

--- module/admin.py
+++ module/admin.py
@@ -14,6 +14,17 @@ class Cog(commands.Cog):
             cloned_channel = await channel.clone(reason="cloneコマンドを使用したため")
             await ctx.send(f"{ctx.author.mention}->{channel.mention}を複製しました！({cloned_channel.mention})")
 
+    @commands.command()
+    async def create_nedoko(self, ctx: commands.Context, owner: discord.Member):
+        if (await YonosumiStaff.is_admin(self.bot, ctx)):
+            
+            making_nedoko_category_id = 800189192637513759
+            making_nedoko: discord.CategoryChannel = self.bot.get_channel(making_nedoko_category_id)
+
+            nedoko: discord.TextChannel = await making_nedoko.create_text_channel(name=f"{owner.name}の寝床")
+            await nedoko.set_permissions(owner, read_messages=True, manage_channels=True, manage_messages=True)
+
+            await ctx.send(content=f"{ctx.author.mention}->`{owner}`の寝床を作成しました。({nedoko.mention})")
 
 def setup(bot):
     bot.add_cog(Cog(bot))
